c7224c2dc03abaa0d9c56a7cf6fda4d28232da91
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-02-25 13:48:14 +0100

    rebuilding site Mon Feb 25 13:48:14 CET 2019

--- update_academic.sh
+++ update_academic.sh
@@ -3,8 +3,8 @@
 # Display available updates to Academic.
 cd themes/academic
 git fetch
-git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
+#git log --pretty=oneline --abbrev-commit --decorate HEAD..origin/master
 cd ../../
 
 # Update Academic.
-git submodule update --remote --merge
+git submodule update --remote --merge themes/academic
