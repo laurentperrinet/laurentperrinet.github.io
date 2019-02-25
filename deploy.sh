805a04ddfff8134c026863f056de2484286d63ca
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-02-25 13:35:24 +0100

    rebuilding site Mon Feb 25 13:35:24 CET 2019

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ git commit -m "$msg"
 git push origin master
 
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub pages repo...\033[0m"
 
 # Build the project.
 hugo -t academic # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -30,7 +30,7 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
-git commit -m "$msg"
+git commit -am "$msg"
 
 # Push source and build repos.
 git push origin master
