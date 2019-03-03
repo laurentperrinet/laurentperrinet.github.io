6ff27511966d9f91faf654b6dec8ab5dfcfe170a
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-03-04 00:13:52 +0100

    rebuilding site Mon Mar  4 00:13:52 CET 2019

--- update_entries.py
+++ update_entries.py
@@ -217,6 +217,7 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
                     parsed_toml['event'] = f'{clean_bibtex_str(entry["booktitle"])}'
                 if 'time_start' in entry:
                     parsed_toml['time_start'] = getDateTimeFromISO8601String(f'{clean_bibtex_str(entry["time_start"])}', full=True)
+                    parsed_toml['date'] = parsed_toml['time_start'] # overwrite date
                 else:
                     parsed_toml['time_start'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
 
