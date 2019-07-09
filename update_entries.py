e13132e2ae27d340662afdb28f21f9ad0cd3c7ed
Laurent Perrinet <laurent.perrinet@gmail.com>
2019-07-09 13:23:38 +0200

    rebuilding site Tue Jul  9 13:23:38 CEST 2019

--- update_entries.py
+++ update_entries.py
@@ -140,7 +140,7 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
             metadata = page.split('+++')
             parsed_toml = toml.loads(metadata[1])
 
-            print('old_key =', old_key, 'dico[new_key]', dico[new_key])
+            print('new_key =', new_key, 'dico[new_key]', dico[new_key])
             old_key = dico[new_key]
             entry = bib_database.entries_dict[old_key]
             #bib_database.get_entry_dict()[old_key]
