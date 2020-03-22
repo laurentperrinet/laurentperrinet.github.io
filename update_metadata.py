c51db116493e041229c6aa5dd497235b7e523bfa
Laurent Perrinet <laurent.perrinet@gmail.com>
2020-03-22 12:08:17 +0100

    Update update_metadata.py

--- update_metadata.py
+++ update_metadata.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 verbose = False
 
 for type in ['Presentations', 'Publications']:#, 'Events']:
@@ -31,54 +33,6 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
 
     import os
     import glob
-    import toml
-
-    # if False: # THIS is BIBTEX CLEANER ...
-    #     from academic import clean_bibtex_str
-    #     # 3- updating bibtex with the metadata
-    #     for file_path in glob.glob('content/publication/**/index.md'):
-    #         new_key = file_path.split('content/publication/')[-1].split('/index.md')[0]
-    #         if not new_key in ['___template___',  'person-re-id', '_index.md']:
-    #             with open(file_path, 'r', encoding='utf-8') as source_file:
-    #                 page = source_file.read() + '\n'
-    #             metadata = page.split('+++')
-    #             parsed_toml = toml.loads(metadata[1])
-    #             #print(parsed_toml)
-    #
-    #             #for new_key in dico.keys():
-    #             old_key = dico[new_key]
-    #             print('input', old_key, ' -> output', new_key)
-    #             #bib_database.get_entry_dict()[old_key]
-    #             translate = {'abstract':'abstract', 'tags':'keywords', 'projects':'projects',
-    #                         'url_pdf':'url', 'url_preprint':'preprint', 'doi':'doi'}
-    #
-    #             if 'date' in parsed_toml.keys():
-    #                 # parsed_toml['date'] = clean_bibtex_str(entry["date"])
-    #                 bib_database.entries_dict[old_key]['year'] = parsed_toml['date'][:4]
-    #
-    #
-    #             for key in ['projects', 'tags']:
-    #                 if key in parsed_toml.keys():
-    #                     tags = parsed_toml[key] #bib_database.entries_dict[old_key][translate['tags']]
-    #                     #print(tags)
-    #                     if not type(tags) == str:
-    #                         parsed_toml[key] = ','.join(tags)
-    #                     #print(parsed_toml['tags'])
-    #
-    #             for key in translate.keys():
-    #                 if key in parsed_toml.keys():
-    #                     if key == 'abstract':
-    #                         if parsed_toml[key] == "": break
-    #                     #print(parsed_toml[key])
-    #
-    #                     bib_database.entries_dict[old_key][translate[key]] = parsed_toml[key]
-    #
-    #     writer = BibTexWriter()
-    #     writer.indent = '    '     # indent entries with 4 spaces instead of one
-    #     writer.order_entries_by = ('ID')
-    #     with open(bibtex, 'w') as bibfile:
-    #         bibfile.write(writer.write(bib_database))
-    #
 
     # 4- updating metadata with bibtex
     from academic.cli import PUB_TYPES, clean_bibtex_str #, clean_bibtex_authors#, clean_bibtex_tags
@@ -132,14 +86,16 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
     elif type == 'Publications':
         pub_dir = 'content/publication'
 
+    import yaml
+
     for file_path in glob.glob(f"{pub_dir}/**/index.md"):
         new_key = file_path.split(f'{pub_dir}/')[-1].split('/index.md')[0]
         if not new_key in ['___template___',  'person-re-id', 'template']:
             with open(file_path, 'r', encoding='utf-8') as source_file:
                 page = source_file.read() + '\n'
-            metadata = page.split('+++')
+            metadata = page.split('---')
             # print(metadata)
-            parsed_toml = toml.loads(metadata[1])
+            parsed_toml = yaml.load(metadata[1])
 
             # print('new_key =', new_key, 'dico[new_key]', dico[new_key])
             old_key = dico[new_key]
@@ -161,8 +117,8 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
 
             if verbose: print('input', old_key, ' -> output', new_key)
 
-            # Prepare TOML front matter for Markdown file.
-            parsed_toml['title'] = clean_bibtex_str(entry["title"])
+            # Prepare yaml front matter for Markdown file.
+            parsed_yaml['title'] = clean_bibtex_str(entry["title"])
 
             if 'date' in entry:
                 date_str = clean_bibtex_str(entry["date"])
@@ -176,12 +132,12 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
                     date_str = f"{date_str}-{month2number(entry['month'])}-01"
                 else:
                     date_str = f"{date_str}-01-01"
-            parsed_toml['date'] = getDateTimeFromISO8601String(date_str)
+            parsed_yaml['date'] = getDateTimeFromISO8601String(date_str)
 
             # Time stamping the entry to be published today
-            if not 'publishDate' in parsed_toml.keys():
+            if not 'publishDate' in parsed_yaml.keys():
                 today = datetime.datetime.now().date().isoformat()#[2:]
-                parsed_toml['publishDate'] = today
+                parsed_yaml['publishDate'] = today
 
             authors = None
             if 'author' in entry:
@@ -190,86 +146,87 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
                 authors = entry['editor']
             if authors:
                 authors = clean_bibtex_authors([i.strip() for i in authors.replace('\n', ' ').split(' and ')])
-                parsed_toml['authors'] = authors #f"[{', '.join(authors)}]"
+                parsed_yaml['authors'] = authors #f"[{', '.join(authors)}]"
 
             for this_key in ['abstract', 'summary']:
                 if this_key in entry:
-                    parsed_toml[this_key] = clean_bibtex_str(entry[this_key])
+                    parsed_yaml[this_key] = clean_bibtex_str(entry[this_key])
                 else:
-                    parsed_toml.pop(this_key, None)
+                    parsed_yaml.pop(this_key, None)
 
             #frontmatter.append(f'featured = {str(featured).lower()}')
 
             if 'keywords' in entry:
-                parsed_toml['tags'] = clean_bibtex_tags(entry["keywords"], normalize)
+                parsed_yaml['tags'] = clean_bibtex_tags(entry["keywords"], normalize)
 
             for this_key in ['grants', 'projects']:
                 if this_key in entry:
-                    # parsed_toml['projects'] = []
-                    parsed_toml[this_key] = clean_bibtex_tags(entry[this_key], normalize)
-                    if verbose: print(parsed_toml[this_key])
-                    # print(parsed_toml['projects'])
+                    # parsed_yaml['projects'] = []
+                    parsed_yaml[this_key] = clean_bibtex_tags(entry[this_key], normalize)
+                    if verbose: print(parsed_yaml[this_key])
+                    # print(parsed_yaml['projects'])
 
 
             for url_key in ['url_slides', 'url_code', 'url_slides']:
                 if url_key in entry:
-                    parsed_toml[url_key] = f'{clean_bibtex_str(entry[url_key])}'
+                    parsed_yaml[url_key] = f'{clean_bibtex_str(entry[url_key])}'
 
             if 'url' in entry:
-                parsed_toml['url_pdf'] = f'{clean_bibtex_str(entry["url"])}'
+                parsed_yaml['url_pdf'] = f'{clean_bibtex_str(entry["url"])}'
 
             if 'preprint' in entry:
-                parsed_toml['url_preprint'] = f'{clean_bibtex_str(entry["preprint"])}'
+                parsed_yaml['url_preprint'] = f'{clean_bibtex_str(entry["preprint"])}'
 
             if 'doi' in entry:
-                parsed_toml['doi'] = f'{entry["doi"]}'
+                parsed_yaml['doi'] = f'{entry["doi"]}'
 
             if type == 'Presentations':
                 # if 'time_start' in entry:
-                #     parsed_toml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["time_start"]))
+                #     parsed_yaml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["time_start"]))
                 # else:
-                #     parsed_toml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
-                #     # parsed_toml['date'] = getDateTimeFromISO8601String('1973-02-23')
+                #     parsed_yaml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
+                #     # parsed_yaml['date'] = getDateTimeFromISO8601String('1973-02-23')
 
                 for this_key in ['event_url', 'location']:
                     if this_key in entry:
-                        parsed_toml[this_key] = f'{clean_bibtex_str(entry[this_key])}'
+                        parsed_yaml[this_key] = f'{clean_bibtex_str(entry[this_key])}'
                 if 'booktitle' in entry:
-                    parsed_toml['event'] = f'{clean_bibtex_str(entry["booktitle"])}'
+                    parsed_yaml['event'] = f'{clean_bibtex_str(entry["booktitle"])}'
 
                 if 'time_start' in entry:
-                    parsed_toml['time_start'] = getDateTimeFromISO8601String(f'{clean_bibtex_str(entry["time_start"])}', full=True)
-                    parsed_toml['date'] = parsed_toml['time_start'] # overwrite date
+                    parsed_yaml['time_start'] = getDateTimeFromISO8601String(f'{clean_bibtex_str(entry["time_start"])}', full=True)
+                    parsed_yaml['date'] = parsed_yaml['time_start'] # overwrite date
                     # HACK to show the entry = Scheduled page publish date.
 
                 else:
-                    #parsed_toml['time_start'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
-                    parsed_toml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
+                    #parsed_yaml['time_start'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
+                    parsed_yaml['date'] = getDateTimeFromISO8601String(clean_bibtex_str(entry["ID"][:10]))
 
 
-                #parsed_toml['publishDate'] = today #str(parsed_toml['date'])[:4] + '-01-01' # overwrite date
+                #parsed_yaml['publishDate'] = today #str(parsed_yaml['date'])[:4] + '-01-01' # overwrite date
                 # remove obsolete entry:
-                parsed_toml.pop("time_start", None)
-                parsed_toml.pop("time_end", None)
+                parsed_yaml.pop("time_start", None)
+                parsed_yaml.pop("time_end", None)
 
 
             elif type == 'Publications':
 
-                parsed_toml['publication_types'] = [f'{PUB_TYPES.get(entry["ENTRYTYPE"], 0)}']
+                parsed_yaml['publication_types'] = [f'{PUB_TYPES.get(entry["ENTRYTYPE"], 0)}']
 
                 # Publication name.
                 if 'booktitle' in entry:
-                    parsed_toml['publication'] = f'*{clean_bibtex_str(entry["booktitle"])}*'
+                    parsed_yaml['publication'] = f'*{clean_bibtex_str(entry["booktitle"])}*'
                 elif 'journal' in entry:
-                    parsed_toml['publication'] = f'*{clean_bibtex_str(entry["journal"])}*'
+                    parsed_yaml['publication'] = f'*{clean_bibtex_str(entry["journal"])}*'
                 else:
-                    parsed_toml['publication'] = ''
+                    parsed_yaml['publication'] = ''
 
                 # TODO:  optional abbreviated version.
                 # publication_short = "In *ICMEW*"
 
 
-            metadata[1] = toml.dumps(parsed_toml)
+            metadata[1] = yaml.dump(parsed_yaml, encoding=('utf-8'), allow_unicode=True)
+
             if not len(metadata) == 3 : print('Z'*150, '  len(metadata)', len(metadata))
 
             # clean-up: strip double lines
@@ -278,7 +235,7 @@ for type in ['Presentations', 'Publications']:#, 'Events']:
             # Save Markdown file.
             try:
                 if verbose: print(f"Saving Markdown to '{file_path}'")
-                page = '+++\n'.join(metadata).strip('\n')
+                page = '---\n'.join(metadata).strip('\n')
                 with open(file_path, 'w', encoding='utf-8') as f:
                     f.write(page + '\n')
             except IOError:
