69d07946746b82154c436d5981035d78b4495150
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-03-31 12:57:34 +0200

    rebuilding site Tue Mar 31 12:57:34 CEST 2020

--- update_gitpages.sh
+++ update_gitpages.sh
@@ -2,7 +2,7 @@
 
 # see https://gohugo.io/hosting-and-deployment/hosting-on-github/
 
-echo "\033[0;32mCommit changes to source repo to GitHub...\033[0m"
+echo -e "\033[0;32mCommit changes to source repo to GitHub...\033[0m"
 
 git add .
 
@@ -15,7 +15,7 @@ git commit -m "$msg"
 git push origin master
 
 
-echo "\033[0;32mDeploying updates to GitHub pages repo...\033[0m"
+echo -e "\033[0;32mDeploying updates to GitHub pages repo...\033[0m"
 
 # Build the project.
 hugo -t academic
